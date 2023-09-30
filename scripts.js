d7148057b1eef9dbc3ee6f3d11a6c9e8a91de2d2
Jopauljose <138276124+Jopauljose@users.noreply.github.com>
2023-09-30 05:21:53 +0000

    created

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Add any JavaScript functionality here
+// This is just a placeholder for demonstration purposes
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Scripts loaded!");
+});
